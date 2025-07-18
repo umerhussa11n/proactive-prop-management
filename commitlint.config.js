bf464b12baadd7679558a49585785ab1fd89f6e2
Umer Hussain <umerhussa11n@gmail.com>
2025-07-18 10:30:12 +0100

    fix: update commit message and commitlint config for better messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,9 +4,9 @@ module.exports = {
   // You can customize rules here if needed.
   // For example, to enforce a maximum header length of 72 characters:
   rules: {
-    'header-max-length': [2, 'always', 72],
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [2, 'always'],
+    'header-max-length': [1, 'always', 100], // Warning instead of error, longer limit
+    'body-leading-blank': [1, 'always'],     // Warning instead of error
+    'footer-leading-blank': [1, 'always'],   // Warning instead of error
     'scope-case': [2, 'always', 'lower-case'],
     'type-enum': [
       2,
@@ -26,4 +26,6 @@ module.exports = {
       ],
     ],
   },
+  // Custom help message for failed commits
+  helpUrl: 'https://www.conventionalcommits.org/'
 };
